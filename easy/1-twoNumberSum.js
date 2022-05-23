dfe8a98773ff093a7c1bfa232eeb9e5f2f8c8f5a
Lawrence Dovin <lawrencedovin@gmail.com>
2022-05-23 09:28:55 -0400

    Added another solution for 2 number sum solution.

--- easy/1-twoNumberSum.js
+++ easy/1-twoNumberSum.js
@@ -8,7 +8,7 @@
 // Sample Output
 // [-1, 11]
 
-// 1.)
+// 1.) O(n^2) time | O(1) space
 // const twoNumberSum = (array, targetSum) => {
 //     for(let i = 0; i < array.length; i++) {
 //         for(let j = i+1; j < array.length; j++){
@@ -21,21 +21,48 @@
 // console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
 
 
-// 2.)
+// // 2.) O(n) time | O(n) space
+// const twoNumberSum = (array, targetSum) => {
+//   // Create a numbers object to store in already iterated values to check if targetNumber already exist while looping.
+//   const numbers = {};
+//   // loop through array using for of loop.
+//   for(number of array) {
+//       // Create variable targetNumber = targetSum - number, to check which number is needed to make up targetSum.
+//       const targetNumber = targetSum - number;
+//       // If targetNumber is in numbers object then return [targetNumber, number];
+//       if(targetNumber in numbers) return [targetNumber, number];
+//       // If targetNumber is not in numbers object store the number to the numbers object.
+//       else numbers[number] = true;
+//   }
+//   // If array is looped without finding the targetNumber in array return [];
+//   return [];
+// }
+
+// console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
+
+// 3.) O(nlog(n)) time | O(1) space
 const twoNumberSum = (array, targetSum) => {
-  // Create a numbers object to store in already iterated values to check if targetNumber already exist while looping.
-  const numbers = {};
-  // loop through array using for of loop.
-  for(number of array) {
-      // Create variable targetNumber = targetSum - number, to check which number is needed to make up targetSum.
-      const targetNumber = targetSum - number;
-      // If targetNumber is in numbers object then return [targetNumber, number];
-      if(targetNumber in numbers) return [targetNumber, number];
-      // If targetNumber is not in numbers object store the number to the numbers object.
-      else numbers[number] = true;
+  // Sort array
+  array = array.sort((a,b) => a-b);
+  // Create Left Pointer and Right Pointer
+  // - Left Pointer starts at index 0
+  // - Right Pointer starts at array.length - 1
+  let left = 0;
+  let right = array.length -1;
+  // While Left < Right if Left position is greater 
+  while(left < right) {
+    // Create currentSum = array[left] + array[right]
+    let currentSum = array[left] + array[right];
+    // If currentSum === targetSum return [array[left], array[right]];
+    if(currentSum === targetSum) return [array[left], array[right]];
+    // If currentSum < targetSum ie. -4 [left] + 11 [right] = 7 [currentSum], targetSum = 10
+    // Then the left pointer needs to increase by 1 so the value of the currentSum increases as well.
+    // Else currentSum > targetSum
+    // Then the right pointer needs to decrease by 1 so the value of the currentSum decreases as well.
+    currentSum < targetSum ? left++ : right++; 
   }
-  // If array is looped without finding the targetNumber in array return [];
-  return [];
+    // If array is looped without finding the targetNumber in array return [];
+    return [];
 }
 
 console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
